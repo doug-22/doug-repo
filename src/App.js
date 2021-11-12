22bf92380452eedcf518de031762f116c1c2304b
Douglas Oliveira <douglas.aguiar410@gmail.com>
2021-11-12 16:18:32 -0300

    Adicionando rota para pagina principal

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Route, BrowserRouter, Routes } from "react-router-dom";
 
+import Main from "./Pages/Main";
 import Home from "./Pages/Home";
 
 export default function App() {
@@ -8,6 +9,7 @@ export default function App() {
   return (
     <BrowserRouter>
             <Routes>
+                <Route path="/" element={< Main/>}/>
                 <Route path="/home" element={< Home/>} />
             </Routes>   
         </BrowserRouter>
