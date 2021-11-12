6950fa259c89e2c42ac71a7fd6fafd7ab12f7ffe
Douglas Oliveira <douglas.aguiar410@gmail.com>
2021-11-12 16:19:45 -0300

    Adicionando destaque na pagina Home

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,17 +1,30 @@
 import React, { useEffect, useState } from "react"
-import "./styles.css"
+import "./style.css"
 import Api from "../../api"
 import ListRow from "../../Components/ListRow"
+import FeaturedProject from "../../Components/FeaturedProject"
 
 const Home = () => {
 
     const [projectList, setProjectList] = useState([]);
+    const [featuredData, setFeaturedData] = useState(null);
 
     useEffect(() => {
         const loadApi = async () => {
+            //pega lista total
             let list = await Api.getHomeList();
-                console.log(list);
+            // console.log(list);
             setProjectList(list);
+
+            //pega o destaque
+            let projetos = list.filter(i=>i.title === "Projetos");
+            console.log(projetos);
+            let randomChosen = Math.floor(Math.random() * (projetos[0].items.length - 1));
+            let chosen = projetos[0].items[randomChosen];
+            setFeaturedData(chosen);
+            //let chosenInfo = await Api.getProjectInfo(chosen._id, "projeto"); Precisar para modal
+
+
         }
 
         loadApi();
@@ -19,6 +32,11 @@ const Home = () => {
 
     return (
         <div className="page">
+            
+            {featuredData &&
+            <FeaturedProject item={featuredData}/>
+            }
+
             <section className="lists">
                 {projectList.map((item, key)=>(
                     <ListRow key={key} title={item.title} items={item.items}/>
