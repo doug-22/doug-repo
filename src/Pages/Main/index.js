092ba75c52d6104e98eb9feff5835b0f6daef415
Douglas Oliveira <douglas.aguiar410@gmail.com>
2021-11-12 16:15:17 -0300

    Criando pagina principal

---
+++ src/Pages/Main/index.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { MdOutlineHome, MdPersonOutline } from "react-icons/md";
+import { Link } from "react-router-dom"
+import "./style.css";
+
+import Logo from "../../Assets/logo.png";
+
+const Main = () => {
+    return (
+        <div className="main-page">
+            <div className="main-header">
+                <img src={Logo} alt="Logo"/>
+            </div>
+            <div className="main-buttons">
+                <div>
+                    <p>Gerenciar acesso:</p>
+                </div>
+                <div className="main-bottons-access">
+                    <Link to="home" className="home-buttom-link">
+                        <div className="home-buttom">
+                            <MdOutlineHome />
+                        </div>
+                        Home
+                    </Link>
+                    <Link to="" className="perfil-buttom-link">
+                        <div className="perfil-buttom">
+                            <MdPersonOutline />
+                        </div>
+                        Perfil
+                    </Link>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Main;
