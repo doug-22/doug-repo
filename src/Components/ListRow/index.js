e2e8b907cd05a297cbf2d11e82650a8d3914312a
Douglas Oliveira <douglas.aguiar410@gmail.com>
2021-11-16 16:06:27 -0300

    Criando funcionalidade de carrossel para a lista de items

--- src/Components/ListRow/index.js
+++ src/Components/ListRow/index.js
@@ -1,13 +1,48 @@
-import React from "react";
+import React, {useState} from "react";
+import { AiOutlineLeft, AiOutlineRight } from "react-icons/ai"
 import "./style.css"
 
 
 const ListRow = ({title, items}) => {
+
+    const [scrollX, setScrollX] = useState(-270);
+
+    const handleLeftArrow = () => {
+        let x = scrollX + Math.round(window.innerWidth / 2);
+        if( x > 0) {
+            x = 0;            
+        }
+        setScrollX(x);
+    }
+
+    const handleRightArrow = () => {
+        let x = scrollX - Math.round(window.innerWidth / 2);
+        // numero 8 precisa ser subtituido por items.length para ficar dinâmica
+        if( (window.innerWidth - (8 * 270)) > x) {
+            x = (window.innerWidth - (8 * 270))
+        }
+        setScrollX(x);
+    }
+
+    console.log(items.length)
+
     return (
         <div className="listRow">
             <h2>{title}</h2>
+
+            <div className="listRow-left" onClick={handleLeftArrow}>
+                <AiOutlineLeft style={{fontSize: 40}}/>
+            </div>
+            <div className="listRow-right" onClick={handleRightArrow}>
+                <AiOutlineRight style={{fontSize: 40}}/>
+            </div>
+
             <div className="listRow-area">
-                <div className="listRow-list">
+                <div className="listRow-list" style={{
+                    marginLeft: scrollX,
+                    // numero 8 precisa ser subtituido por items.length para ficar dinâmica
+                    width: 8 * 270
+                }}>
                     {items.length > 0 && items.map((item, key) => (
                         <div key={key} className="listRow-item">
                             <img src={item.imagem} alt={item.titulo}/>
