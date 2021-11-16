86ed58c8f1ee874c512acd2de26ba9aaa6c1b9d9
Douglas Oliveira <douglas.aguiar410@gmail.com>
2021-11-16 17:50:23 -0300

    Limitando a descrição do destaque

--- src/Components/FeaturedProject/index.js
+++ src/Components/FeaturedProject/index.js
@@ -3,6 +3,12 @@ import { BsFillPlayFill, BsInfoCircle } from "react-icons/bs"
 import "./style.css";
 
 const FeaturedProject = ({item}) => {
+
+    let description = item.descricao;
+    if(description.length > 200) {
+        description = description.substring(0, 200) + "..."
+    }
+
     return (
         <section className="featured" style={{
             backgroundSize: "cover",
@@ -13,7 +19,7 @@ const FeaturedProject = ({item}) => {
                 <div className="featured-horizontal">
                     <div className="featured-name">{item.titulo}</div>
                     <div className="featured-description">
-                        {item.descricao}
+                        {description}
                     </div>
                     <div className="featured-buttons">
                         <a className="featured-watch-button" href={item.link} target="_blank" rel="noreferrer"><BsFillPlayFill className="featured-icon-button"/>Acessar</a>
